5cdd059c4ac869e8acbb443b59abf47aa8d84075
jayhan <han6343@gmail.com>
2020-08-23 19:27:21 +0900

    2020.08.22 Hot Issue

--- app/src/main/java/com/example/todaysshow/fragment/community/CommunityHotIssue.kt
+++ app/src/main/java/com/example/todaysshow/fragment/community/CommunityHotIssue.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.GridView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todaysshow.R
+import com.example.todaysshow.`object`.Journal
+import com.example.todaysshow.adapter.CommunityHotIssueJournalAdapter
 
 class CommunityHotIssue : Fragment() {
 
@@ -21,6 +25,63 @@ class CommunityHotIssue : Fragment() {
     ): View? {
         val viewGroup: ViewGroup = inflater.inflate(R.layout.activity_community_hot_issue_fragment, null) as ViewGroup
 
+        var communityHotIssueRecyclerView : RecyclerView = viewGroup.findViewById(R.id.community_hot_issue_rv)
+        communityHotIssueRecyclerView.layoutManager = LinearLayoutManager(context)
+        communityHotIssueRecyclerView.adapter = CommunityHotIssueJournalAdapter(getJournals(), context!!)
+
         return viewGroup
+    }
+
+    fun getJournals() : ArrayList<Journal> {
+        val journals = java.util.ArrayList<Journal>()
+        journals.add(
+            Journal(
+                "2020\n공연트렌트",
+                R.drawable.journal_image_sample1
+            )
+        )
+        journals.add(
+            Journal(
+                "샤롯데 씨어\n첫! 방문기",
+                R.drawable.journal_image_sample2
+            )
+        )
+        journals.add(
+            Journal(
+                "나는\n도대체 어디...?",
+                R.drawable.journal_image_sample1
+            )
+        )
+        journals.add(
+            Journal(
+                "4대 뮤지컬\n오페라의 유령",
+                R.drawable.journal_image_sample2
+            )
+        )
+        journals.add(
+            Journal(
+                "2020\n공연트렌트",
+                R.drawable.journal_image_sample1
+            )
+        )
+        journals.add(
+            Journal(
+                "샤롯데 씨어\n첫! 방문기",
+                R.drawable.journal_image_sample2
+            )
+        )
+        journals.add(
+            Journal(
+                "나는\n도대체 어디...?",
+                R.drawable.journal_image_sample1
+            )
+        )
+        journals.add(
+            Journal(
+                "4대 뮤지컬\n오페라의 유령",
+                R.drawable.journal_image_sample2
+            )
+        )
+        return journals
     }
 }
